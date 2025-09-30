e1ea3fac32e76ab877f1d734f7f304dbae9490dc
OsbornChen <osbornc@nycstudents.net>
2025-09-30 11:48:39 -0400

    3rd

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,12 @@
 public class Main {
-    public class AppointmentBook {
-        private boolean[][] schedule;
-        public AppointmentBook(boolean[][] schedule) {
-            this.schedule = schedule;
-        }
-        private boolean isMinuteFree(int period, int minute) {
-            return schedule[period - 1][minute];
-        }
-        public int findFreeBlock(int period, int duration){
-            return 0;
-        }
-        public boolean makeAppointment(int startPeriod, int endPeriod, int duration) {
-            return false;
-        }
+    public static void main(String[] args) {
         Boolean[][] schedule = new Boolean[8][60];
-        AppointmentBook a = new AppointmentBook(schedule);
+        AppointmentBook.AppointmentBook a = new AppointmentBook.AppointmentBook(schedule);
         for(int i = 10; i < 15; i++) schedule[1][i] = true;
         public void printPeriod(int period){
             for(int 1 = 0; i < schedule[period - 1].length; i++)
-                System.out.println(i + " " + schedule[period - 1][i]);
+            System.out.println(i + " " + schedule[period - 1][i]);
         }
+
     }
 }
